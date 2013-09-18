ba802dc5764f8e394a59cda4c4765db94b15a1a9
Apolo Doutrinador <marcelo@lema21.com.br>
2013-09-18 12:55:30 -0300

    form

---
+++ s3-upload/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Example Upload to S3 in PHP</title>
+</head>
+<body>
+<h1>Upload file to S3</h1>
+<form action="upload.php" method="post" enctype="multipart/form-data">
+    <input type="text" name="description" />
+    <input type="file" name="file" />
+    <input type="submit/>
+</body>
+</html>
+
